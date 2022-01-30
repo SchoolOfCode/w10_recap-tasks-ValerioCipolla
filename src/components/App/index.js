722ad8f5340ec84e464e01435caa8cc87985ddf5
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-30 10:40:30 +0000

    added style to login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,8 +6,11 @@ import { nanoid } from "nanoid";
 import LoginButton from "../LoginButton";
 import LogoutButton from "../LogoutButton";
 import Profile from "../Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import PreLoginSection from "../PreLoginSection";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   const [articles, setArticles] = useState(null);
 
   useEffect(() => {
@@ -18,21 +21,29 @@ function App() {
     <>
       <LoginButton />
       <LogoutButton />
-      <Profile />
-      <div className="App">
-        <Title text="WikiPigeon" />
-        {articles &&
-          articles.map((article) => {
-            return (
-              <Article
-                key={nanoid()}
-                title={article.title}
-                paragraphs={article.paragraphs}
-                comments={article.comments}
-              />
-            );
-          })}
-      </div>
+      <Profile
+        user={user}
+        isAuthenticated={isAuthenticated}
+        isLoading={isLoading}
+      />
+      {isAuthenticated ? (
+        <div className="App">
+          <Title text="WikiPigeon" />
+          {articles &&
+            articles.map((article) => {
+              return (
+                <Article
+                  key={nanoid()}
+                  title={article.title}
+                  paragraphs={article.paragraphs}
+                  comments={article.comments}
+                />
+              );
+            })}
+        </div>
+      ) : (
+        <PreLoginSection />
+      )}
     </>
   );
 }
---
+++ src/components/PreLoginSection/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PreLoginSection = () => {
+  return (
+    <section>
+      <h1>Please login to see articles.</h1>
+    </section>
+  );
+};
+
+export default PreLoginSection;
